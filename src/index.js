6f92a8af12ef3d04afa67ccc9ce58f1c1291fff6
Guptaabhii <112747867+Guptaabhii@users.noreply.github.com>
2023-06-11 22:44:44 +0530

    chore: added backend

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { makeServer } from "./server";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+makeServer();
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
@@ -14,4 +16,5 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
 reportWebVitals();

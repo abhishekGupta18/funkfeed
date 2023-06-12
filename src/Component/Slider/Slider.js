9046b1627b8616597b070cbf16f5c772bcf16f55
Guptaabhii <112747867+Guptaabhii@users.noreply.github.com>
2023-06-13 00:57:15 +0530

    style: styled login page

---
+++ src/Component/Slider/Slider.js
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
+const slides = [
+  "https://images.pexels.com/photos/6146929/pexels-photo-6146929.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  "https://images.pexels.com/photos/5935228/pexels-photo-5935228.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  "https://images.pexels.com/photos/4555321/pexels-photo-4555321.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  "https://images.pexels.com/photos/2694434/pexels-photo-2694434.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+];
+
+function Slider() {
+  const [selectedImage, setSelectedImage] = useState(0);
+
+  useEffect(() => {
+    setInterval(() => {
+      setSelectedImage((selectedImage) =>
+        selectedImage < 3 ? selectedImage + 1 : 0
+      );
+    }, 4000);
+  }, []);
+
+  return (
+    <div className="slider-container">
+      <Link to="/store">
+        <img src={slides[selectedImage]} alt={`banner${selectedImage + 1}`} />
+      </Link>
+    </div>
+  );
+}
+
+export default Slider;
--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,8 +1,38 @@
+import Slider from "../../Component/Slider/Slider";
+import Logo from "../../Asset/final_logo.png";
+
 export const Login = () => {
   return (
-    <>
-      {" "}
-      <h1 className="text-2xl">this is login page</h1>
-    </>
+    <div className="flex flex-row h-screen items-center justify-center px-[5rem]   ">
+      <div className="w-[50%] rounded-[0.5rem] overflow-hidden">
+        <Slider />
+      </div>
+
+      <form className=" w-[50%]  flex flex-col justify-start items-center gap-[1.5rem]">
+        <img src={Logo} width={200} height={100} />
+        <label className="flex flex-col items-start gap-[0.5rem]">
+          Username{" "}
+          <input
+            type="text"
+            placeholder="username"
+            className="border-solid border-[1px] border-[#271c19] px-[1rem] py-[0.3rem] rounded-[0.5rem]"
+          />
+        </label>
+        <label className="flex flex-col items-start gap-[0.5rem]">
+          Password{" "}
+          <input
+            type="text"
+            placeholder="password"
+            className="border-solid border-[1px] border-[#271c19] px-[1rem] py-[0.3rem] rounded-[0.5rem]"
+          />
+        </label>
+        <button className="text-[#271c19] border-solid border-[1px] border-[#271c19] px-[1rem] py-[0.5rem] font-bold rounded-[0.5rem] hover:bg-[#55423d] hover:text-[#ffff]">
+          Login
+        </button>
+        <button className=" text-[#271c19] border-solid border-[1px] border-[#271c19] px-[1rem] py-[0.5rem] font-bold rounded-[0.5rem] hover:bg-[#55423d] hover:text-[#ffff]">
+          Login as guest
+        </button>
+      </form>
+    </div>
   );
 };
